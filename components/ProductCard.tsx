67b2c3128bedad6211a62b7e13ef6cf5a6511753
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-11-07 00:25:45 +0545

    Create ProductCard.tsx

---
+++ components/ProductCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Product } from "@/data/products";
+import Link from "next/link";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const discountedPrice = product.discount
+    ? product.price - (product.price * product.discount) / 100
+    : product.price;
+
+  return (
+    <div
+      key={product.id}
+      className="bg-white rounded-lg shadow-md overflow-hidden"
+    >
+      <img
+        src={product.image}
+        alt={product.name}
+        width={256}
+        height={180}
+        className="w-full h-48 object-cover"
+      />
+      <div className="p-4">
+        <h2 className="font-bold text-xl mb-2">{product.name}</h2>
+        <p className="text-gray-600 text-sm mb-2">{product.description}</p>
+        <p className="text-gray-700 text-sm">Spice Level: {product.spiceLevel}</p>
+        <div className="flex items-baseline mt-2">
+          {product.discount ? (
+            <p className="text-lg font-semibold text-gray-800 mr-2">
+              ${discountedPrice.toFixed(2)}
+            </p>
+          ) : null}
+          <p
+            className={`text-gray-600 ${product.discount ? "line-through" : "font-semibold text-lg"}`}
+          >
+            ${product.price.toFixed(2)}
+          </p>
+        </div>
+        <p className="text-sm text-gray-500 mt-1">Stock: {product.stock}</p>
+        <Link href={`/product/${product.slug}`}>
+          <button className="mt-4 bg-primary hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            View Details
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
