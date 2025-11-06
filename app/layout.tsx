443610bba2b2f33f89f592398c1f3ac7515fda41
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-11-06 23:45:26 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,19 @@
-import "./globals.css";
+import "@/app/globals.css";
+import Link from "next/link";
+import Footer from "@/components/Footer";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About Us", href: "/about" },
+  { name: "Products", href: "/products" },
+  { name: "Contact", href: "/contact" },
+];
 
 export default function RootLayout({
   children,
@@ -6,10 +21,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      >
+    <html lang="en" className={roboto.className}>
+      <body>
+        <nav className="bg-white p-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <Link
+              href="/"
+              className="text-black text-2xl font-bold flex items-center"
+            >
+              <img
+                src="/logo.png"
+                alt="Ranjana Arai's Pickles Logo"
+                className="h-16 w-auto"
+              />
+            </Link>
+            <div className="space-x-4">
+              {navItems.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className="text-black text-lg hover:text-primary"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </nav>
         {children}
+        <Footer />
       </body>
     </html>
   );
