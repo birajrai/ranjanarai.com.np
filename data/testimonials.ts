1e8f819a84a32b2fc750d4b528ecf4e4ab8aeb46
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-11-07 01:06:56 +0545

    Create testimonials.ts

---
+++ data/testimonials.ts
@@ -0,0 +1,17 @@
+export const testimonials = [
+  {
+    id: 1,
+    quote: "These pickles are simply the best!",
+    author: "Priya Sharma",
+  },
+  {
+    id: 2,
+    quote: "A true taste of home!",
+    author: "Rajesh Singh",
+  },
+  {
+    id: 3,
+    quote: "I've tried many, but Ranjana Didi Ko Aachar are my absolute favorite.",
+    author: "Anjali Gurung",
+  },
+];
